ebe6cd4e4c9c6c2202e223968e16f18a1144ffa5
Tugs Boy <social_media_bs@protonmail.com>
2023-03-18 10:43:33 -0500

    pointer, and variable practice

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.3
+- Playing with pointers and variables
+- [practicing this](https://www.youtube.com/watch?v=yyUHQIec83I&t=3477s)
 ## 0.0.2
 - Testing permissions
 ## 0.0.1
--- src/main.go
+++ src/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi.\nHow is it going bud?\nYou okay??")
+	const conferenceTickets uint = 50
+	conferenceName := "Go Conference"
+	var remainingTickets uint = 50
+	var firstName string
+	var lastName string
+	var email string
+	var numTickets uint
+	pointRemainingTickets := &remainingTickets
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&email)
+	fmt.Println("Please enter the amount of tickets you would like:")
+	fmt.Scan(&numTickets)
+	fmt.Printf("%v %v you have purchased %v tickets.  We will send conformation to: %v\n", firstName, lastName, numTickets, email)
+
+	*pointRemainingTickets = remainingTickets - numTickets
+
+	fmt.Printf("There are %v tickets still available.\n", remainingTickets)
 }
